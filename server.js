55d72837283c41049e78c774e15e619ef372764f
fieldsierra <fieldsierra@email.arizona.edu>
2022-06-11 22:26:50 -0700

    poopy

--- server.js
+++ server.js
@@ -9,6 +9,7 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const parser = require("body-parser");
+const poopy = "poppy";
 const app = express()
 app.use(parser.json() );
 const port = 3000
